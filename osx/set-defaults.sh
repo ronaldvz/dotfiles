08194a1491adfa2a4fd51af994ef410d5ea6bf02
Ronald van Zuijlen <ronald@siteworkers.nl>
2015-01-30 09:33:35 +0100

    Small alteration to osx defaults.

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -33,7 +33,7 @@ defaults write com.apple.LaunchServices LSQuarantine -bool false
 ###############################################################################
 
 # Disable press-and-hold for keys in favor of key repeat
-defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool true
 
 # Set a blazingly fast keyboard repeat rate
 defaults write NSGlobalDomain KeyRepeat -int 0

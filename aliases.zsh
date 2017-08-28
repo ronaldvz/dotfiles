2a68ab23d804b4810a8a1d1234f8d385192693da
Ronald van Zuijlen <ronaldvanzuijlen@gmail.com>
2017-08-28 21:29:00 +0200

    Remove old files

--- aliases.zsh
+++
@@ -1,6 +0,0 @@
-# Aliases, ln -s this to .oh-my-zsh/custom/aliases.zsh
-alias valias="vim ~/.oh-my-zsh/custom/aliases.zsh"
-alias dot="cd ~/.dotfiles && ls -l"
-alias pr="cd ~/Projects && ls -l"
-alias i3conf="vim ~/.i3/config"
-
